632e4aefb54e7f9f58b8a29e199a8d0a4f83c30a
Jose Luis <wildchamo@gmail.com>
2025-04-20 11:50:53 -0500

    dev server up n running

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, Debería subirme!')
+  })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  }
+)
