a7d2909a107525c6f96601aac14ea40173778b1b
Jose Luis <wildchamo@gmail.com>
2025-04-20 13:36:19 -0500

    use buildpacks to deploy

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Debería subirme!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
