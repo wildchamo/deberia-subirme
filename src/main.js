71facbc4619287e1f300eed77331a262774aa3b8
Jose Luis <wildchamo@gmail.com>
2025-04-20 12:28:03 -0500

    dockerize app for cloud run

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import express from 'express';
 
-
-
 const app = express();
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, Debería subirme!')
