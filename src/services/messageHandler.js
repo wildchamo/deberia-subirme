f5d517fc02ce8954844502f60eeebdea7fde20c8
Jose Luis <wildchamo@gmail.com>
2025-04-25 23:21:46 -0500

    sending messages!

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -3,18 +3,32 @@ import whatsappService from "./whatsappService.js";
 class MessageHandler {
   constructor() {
     this.reportForm = {};
+    this.reportQuery = {};
   }
 
   async handleIncomingMessage(message, senderInfo) {
+    await whatsappService.markAsRead(message.id);
+
     switch (message?.type) {
       case "text":
         const incomingMessage = message.text.body.toLowerCase().trim();
+
+        if (this.reportQuery[message.from]) {
+          return await this.handleQueryFlow(message.from, incomingMessage);
+        }
+
+        if (this.reportForm[message.from]) {
+          return await this.handleReportFlow(message.from, incomingMessage);
+        }
 
         await this.sendWelcomeMessage(message.from, message.id);
         await this.sendWelcomeMenu(message.from);
 
         break;
       case "interactive":
+        const chosenOption = message.interactive.button_reply.id.trim();
+        await this.handleMenuOption(message.from, chosenOption);
+
         break;
 
       default:
@@ -49,6 +63,32 @@ class MessageHandler {
 
     console.log(title, buttons, to);
     await whatsappService.sendInteractiveButtons(to, title, buttons);
+  }
+
+  async handleMenuOption(to, option) {
+    let response;
+
+    switch (option) {
+      case "hacer-consulta":
+        this.reportQuery[to] = { step: "plate" };
+
+        response =
+          "¡Claro! Por favor, ingresa la placa del vehículo en el formato *ABC-123* para continuar.";
+        break;
+
+      //TODO
+      case "reportar-incidente":
+        this.reportQuery[to] = { step: "category" };
+        response =
+          "Gracias por tu confianza. \nPara entender mejor lo que ocurrió, por favor selecciona la categoría que mejor describe la situación.";
+        break;
+
+      default:
+        response =
+          "Lo siento, no entendí tu selección, elige una opción del menú";
+    }
+
+    await whatsappService.sendMessage(to, response);
   }
 }
 
